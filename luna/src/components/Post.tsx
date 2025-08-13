0cce96ef83812fccc022c896159a146b1557358f
Peter Ejiofor <tercrft@gmail.com>
2025-08-13 12:43:25 +0100

    Added card components to Creators and Community page

---
+++ luna/src/components/Post.tsx
@@ -0,0 +1,61 @@
+import Card from "./Card";
+
+    const community = [
+        {name: "Creator Name",
+        image:"",
+         likes:"2",
+          comments: [{text:""}]
+        },
+
+        {name: "Creator Name",
+        image:"",
+         likes:"2",
+          comments: [{text:""}]
+        }
+
+    ]
+
+interface PostItems {
+    name?:string;
+    image?:string;
+    likes?:number;
+    comments?:string[]
+}
+
+const Post = ({name="Default"}:PostItems) =>{
+    return(
+        <>
+            <section>
+                {community.map((com) => (
+                    <div className="mt-[70px] mb-[100px] border-b border-b-[2px] pb-[40px]">
+
+                        {/* Top section with icon and creator name*/}
+                        <article className="flex flex-row gap-10 w-[308px]">
+                            <div className="flex-1 ">
+                                <Card custom="h-[100px] w-[100px]" round={true}/>
+                            </div>
+                            <h2 className="flex flex-col justify-center">{com.name}</h2>
+                        </article>
+
+                        {/* actual post image*/}
+                        <article className="mt-[30px]">
+
+                            <Card custom="h-[459px] w-[867px]" round={true}/>
+                        </article>
+
+                        {/* Like and comment buttons*/}
+                        <article className="flex flex-row gap-5 mt-[30px]">
+                            <Card custom="h-[48px] w-[48px]" round={true}/>
+                            <Card custom="h-[48px] w-[48px]" round={true}/>
+                        </article>
+                        
+                    </div>
+
+                ))}
+                
+            </section>
+        </>
+    )
+}
+
+export default Post;
--- luna/src/components/pages/Creators.tsx
+++ luna/src/components/pages/Creators.tsx
@@ -1,3 +1,5 @@
+import Card from "../Card";
+
 const Creators = () => {
     const creators = [
         {name: "Name",
@@ -28,10 +30,7 @@ const Creators = () => {
 
         <section className="flex flex-row w-full justify-evenly mt-[30px]">
             {creators.map((creator) => (
-                <div className="border bg-black h-[431px] w-[166px]">
-                    <h2>{creator.name}</h2>
-                    <h4>{creator.user}</h4>
-                </div>
+                <Card custom="h-[431px] w-[166px]"/>
             )
             )}
         </section>
@@ -39,21 +38,17 @@ const Creators = () => {
         <section className="mt-[70px]">
             <h2> Updates </h2>
             <article className="flex flex-row justify-evenly mt-[30px]">
-                <div className="w-[454px] h-[263px] border bg-black">
-
-                </div>
-                <div className="w-[454px] h-[263px] border bg-black">
-
-                </div>
+                <Card custom="w-[454px] h-[263px]"/>
+                <Card custom="w-[454px] h-[263px]"/>
             </article>
         </section>
 
         <section className="mt-[70px]">
             <h2>Shop</h2>
             <article className="flex flex-row justify-evenly mt-[30px]">
-                <div className="rounded-2xl h-[280px] w-[280px] bg-black"></div>
-                <div className="rounded-2xl h-[280px] w-[280px] bg-black"></div>
-                <div className="rounded-2xl h-[280px] w-[280px] bg-black"></div>
+                <Card custom="w-[280px] h-[280px]" round={true} />
+                <Card custom="w-[280px] h-[280px]" round={true}/>
+                <Card custom="w-[280px] h-[280px]" round={true}/>
             </article>
         </section>
         </>
